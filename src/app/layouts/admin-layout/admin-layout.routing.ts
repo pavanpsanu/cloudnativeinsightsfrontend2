dbff3d2d35c4f9e06f5e5961a2fba72df05110ff
pavan sanu <pavanp@gmail.com>
2022-09-20 21:30:28 +0000

    first commit

---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,30 @@
+import { Routes } from "@angular/router";
+
+import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
+import { IconsComponent } from "../../pages/icons/icons.component";
+import { MapComponent } from "../../pages/map/map.component";
+import { NotificationsComponent } from "../../pages/notifications/notifications.component";
+import { UserComponent } from "../../pages/user/user.component";
+import { TablesComponent } from "../../pages/tables/tables.component";
+import { TypographyComponent } from "../../pages/typography/typography.component";
+// import { RtlComponent } from "../../pages/rtl/rtl.component";
+import { ServicesComponent } from "../../pages/services/services.component";
+// import {ReportComponent} from "../../pages/Report/ReportComponent";
+// import {CustomerComponent} from "../../pages/customer/CustomerComponent";
+
+
+
+
+export const AdminLayoutRoutes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "icons", component: IconsComponent },
+  { path: "maps", component: MapComponent },
+  { path: "notifications", component: NotificationsComponent },
+  { path: "user", component: UserComponent },
+  { path: "tables", component: TablesComponent },
+  { path: "typography", component: TypographyComponent },
+  { path: "services", component: ServicesComponent },
+  // { path: "customer", component: CustomerComponent },
+  // { path: "report", component: ReportComponent },
+  // { path: "rtl", component: RtlComponent }
+];
